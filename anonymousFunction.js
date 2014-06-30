285f565ef2c70916c7a9ecd64915b5e189a99bbf
Kyle Maune <kmaune71@gmail.com>
2014-06-30 13:34:10 -0700

    added anonymous function example

---
+++ anonymousFunction.js
@@ -0,0 +1,15 @@
+// anonymousFunction.js
+// (C) 2014 splashinn
+
+// Create a new anonymous function, to use as a wrapper
+(function(){
+    // The variable that would, normally, be global
+    var msg = "Thanks for visiting!";
+
+    // Binding a new function to a global object
+    window.onunload = function(){
+        // Which uses the 'hidden' variable
+        alert( msg );
+    };
+// Close off the anonymous function and execute it
+})();
