c8fbb61790299e529e90109685a8006aa9d4d020
Kyle Maune <kmaune71@gmail.com>
2014-07-01 17:55:48 -0700

    added find sequence file

---
+++ findSequence.js
@@ -0,0 +1,17 @@
+// findSequence.js
+// (C) 2014 splashinn
+
+function findSequence(goal) {
+  function find(start, history) {
+    if (start == goal)
+      return history;
+    else if (start > goal)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
+
+print(findSequence(24));
