3ba1e14d19a41b3aa411d7bb64294346fc221b2f
Kyle Maune <kmaune71@gmail.com>
2014-07-04 14:03:49 -0700

    added file
    a function that finds the first non-repeated character in a string

---
+++ repeater.js
@@ -0,0 +1,21 @@
+// repeater.js
+// (C) 2014 splashinn
+
+// Write a function to find the first non-repeated character in a string.
+function repeater(string) {
+  var charCount = {};
+  for(var i = 0; i < string.length; i++) {
+    if(charCount[string[i]]) {
+      charCount[string[i]] "More Than One";
+    } else {
+        charCount[string[i]] "One Time";
+    }
+  }
+  for(var j = 0; j < string.length; j++) {
+    if(charCount[string[j]] === "One Time") {
+      return string.charAt(j);
+    }
+  }
+
+  return "Everything is repeated";
+}
