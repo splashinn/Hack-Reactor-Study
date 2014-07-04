7090327b4089ab479077c0dbd20cd27f8b0320c9
Kyle Maune <kmaune71@gmail.com>
2014-07-04 14:36:50 -0700

    added string reduction file

---
+++ stringReduction.js
@@ -0,0 +1,34 @@
+// stringReduction.js
+// (c) 2014 splashinn
+
+// Have the function StringReduction(str) take the str parameter being passed
+// and return the smallest number you can get through the following reduction
+// method. The method is: Only the letters a, b, and c will be given in str and
+// you must take two different adjacent characters and replace it with a third.
+
+
+var replacements =
+{
+  "ab" : "c",
+  "ac" : "b",
+  "bc" : "a",
+  "ba" : "c",
+  "ca" : "b",
+  "cb" : "a"
+};
+
+function StringReduction(s)
+{
+  var original, key;
+  while( s != original )
+  {
+   original = s;
+   for( key in replacements )
+      s = s.replace( key , replacements[key] );
+  }
+  return s.length;
+}
+
+// keep this function call here
+// to see how to enter arguments in JavaScript scroll down
+StringReduction(readline());
