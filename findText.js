e26beeaabb45e2c63e4dd626d1f579689763cc32
Kyle Maune <kmaune71@gmail.com>
2014-07-04 19:14:58 -0700

    added simple text find file
    very simple example of how JavaScript can be used to find text in a
    paragraph. Could use updates and modifications.
    
    Inspired by Codecademy JS course.

---
+++ findText.js
@@ -0,0 +1,20 @@
+// findText.js
+// (C) 2014 splashinn
+
+var text = "blah blah blah blah blah blah blah Kyle blah blah blah blah Kyle blah blah Kyle";
+var myName = "Kyle";
+var hits = [];
+
+for (var i = 0; i < text.length; i++) {
+    if (text[i] === "K") {
+        for (var j = i; j < myName.length + i; j++) {
+            hits.push(text[j]);
+        }
+    }
+}
+
+if (hits.length === 0) {
+    console.log("Your name wasn't found");
+} else {
+    console.log(hits);
+}
